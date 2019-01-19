5862c39dcd36f6c6efaf757343f276f08c879334
Rudi Kershaw <rudi.kershaw@cognitran.com>
2019-01-19 22:29:31 +0000

    Create logo, update config, and add settings toggle.

--- mobile-app/www/js/index.js
+++ mobile-app/www/js/index.js
@@ -2,7 +2,6 @@ var app = new (function() {
 
     var select = document.querySelector.bind(document);
     var selectAll = document.querySelectorAll.bind(document);
-    var storage = window.localStorage;
 
     var domain = "";
     var uuid = "";
@@ -14,25 +13,18 @@ var app = new (function() {
 
     // deviceready Event Handler
     this.onDeviceReady = function() {
-        domain = storage.getItem('domain');
-        uuid = storage.getItem('uuid');
-        if (domain && uuid && uuid.length > 0) {
-            showAlarmStatus();
-        } else {
-            showDetailsForm();
-        }
-    };
+        domain = window.localStorage.getItem('domain');
+        uuid = window.localStorage.getItem('uuid');
 
-    var showDetailsForm = function() {
-        selectAll('.form-container')[0].classList.remove('hide');
-        select('#save-details-button').addEventListener("click", function(event){
+        select('#save-details-button').addEventListener("click", function(event) {
             event.preventDefault();
             domain = select('#domain-details-input').value;
             uuid = select('#uuid-details-input').value;
             if (domain && uuid) {
-                storage.setItem('domain', domain);
-                storage.setItem('uuid', uuid);
-                selectAll('.form-container')[0].classList.add('hide');
+                standardizeDomain();
+                window.localStorage.setItem('domain', domain);
+                window.localStorage.setItem('uuid', uuid);
+                select('.form-container').classList.add('hide');
                 showAlarmStatus();
             } else {
                 var inputs = selectAll('.form-container input');
@@ -41,10 +33,56 @@ var app = new (function() {
                 }
             }
         });
+
+        select('button.settings').addEventListener("click", function(event){
+            showDetailsForm();
+        });
+
+        if (domain && uuid && uuid.length > 0) {
+            showAlarmStatus();
+        } else {
+            showDetailsForm();
+        }
+    };
+
+    var showDetailsForm = function() {
+        select('#domain-details-input').value = domain;
+        select('#uuid-details-input').value = uuid;
+        select('button.settings').classList.add('hidden');
+        select('.form-container').classList.remove('hide');
+        select('.alarm-status').classList.add('hide');
+    };
+
+    var standardizeDomain = function() {
+        if (domain.indexOf('/', domain.length - 1) === -1) {
+            domain = domain + '/';
+        }
+
+        if (!(new RegExp("^(http|https)://", "i").test(domain))) {
+            domain = 'http://' + domain;
+        }
     };
 
     var showAlarmStatus = function() {
-        selectAll('.alarm-status')[0].classList.remove('hide');
+        select('.alarm-status p').innerHTML = 'Loading...';
+        showStatusMessage();
+        select('button.settings').classList.remove('hidden');
+        select('.alarm-status').classList.remove('hide');
+    };
+
+    var showStatusMessage = function() {
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', domain + 'water-alarm/query/' + uuid);
+        ajax.onload = function() {
+            if (ajax.status === 200) {
+                select('.alarm-status p').innerHTML = '200';
+            } else if (ajax.status === 404) {
+                select('.alarm-status p').innerHTML = '404';
+            } else {
+                select('.alarm-status p').innerHTML = 'Something went wrong';
+            }
+        };
+        ajax.send();
     };
 })();
 
