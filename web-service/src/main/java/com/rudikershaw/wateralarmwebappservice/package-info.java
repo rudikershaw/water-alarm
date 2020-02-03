91fba7eca0ea7afe9a7a0ea0d65f8299215957e0
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2020-02-03 22:29:22 +0000

    Remove now redundant project sections and ammend main README.md

--- web-service/src/main/java/com/rudikershaw/wateralarmwebappservice/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Package containing classes for monitoring the statuses of water alarms that report to the water alarm REST
- * controller web service (also contained in this package).
- */
-package com.rudikershaw.wateralarmwebappservice;
